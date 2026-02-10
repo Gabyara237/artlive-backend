87e964ecd894e754306739628d55cf732c4b269c
Gabriela Araujo <gabyara237@gmail.com>
2026-02-09 18:49:54 -0800

    feat: implement sign-in endpoint

--- routers/auth.py
+++ routers/auth.py
@@ -19,8 +19,6 @@ def sign_up():
         cursor.execute(" SELECT * FROM users WHERE username = %s ", (new_user_data["username"],))
         existing_username = cursor.fetchone()
         if existing_username:
-            cursor.close()
-            connection.close()
             return jsonify({"err":"Username already taken"}),409
         
         
@@ -30,7 +28,6 @@ def sign_up():
         cursor.execute("INSERT INTO users (username, password, role) VALUES (%s,%s, %s) RETURNING id, username, role", (new_user_data["username"], hashed_password.decode('utf-8'), new_user_data["role"]))
         create_user = cursor.fetchone()
         connection.commit()
-        connection.close()
 
         payload ={
             "username": create_user["username"], 
@@ -41,6 +38,52 @@ def sign_up():
         token = jwt.encode({"payload":payload}, os.getenv('JWT_SECRET'))
         return jsonify({"token":token}),201
     
-
     except Exception as err:
         return jsonify({"err": str(err)}),401
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+@auth_blueprint.route('/auth/sign-in', methods=["POST"])
+def sign_in():
+
+    connection = None
+    cursor = None
+    try:
+        user_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor( cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (user_data["username"],))
+
+        existing_user= cursor.fetchone()
+        if existing_user is None:
+            return jsonify({"err": "Invalid credentials."}), 401
+        password_is_valid = bcrypt.checkpw( bytes(user_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+        
+        if not password_is_valid:
+            return jsonify({"err": "Invalid credentials"}),401
+        
+        payload ={
+            "username": existing_user["username"], 
+            "id": existing_user["id"]
+        }
+
+        token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
+
+
+        return jsonify({"token": token}), 200
+    
+    except Exception as err:
+        return jsonify({"err": str(err)}),401
+    
+    finally:
+        if cursor:
+            cursor.close()
+
+        if connection:
+            connection.close()
